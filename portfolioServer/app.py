c9b489985bcee454df84a2dbe57975fcc6d419c6
Nobert-Muma <mumanobert17@gmail.com>
2024-10-29 01:23:21 +0300

    import os

--- portfolioServer/app.py
+++ portfolioServer/app.py
@@ -3,7 +3,7 @@ from models.message import Message
 from flask import Flask, request
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+import os
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///messages.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
